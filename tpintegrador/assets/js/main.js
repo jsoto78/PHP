a0342c43a33dc7c26db77cf621b2fae86ca91dd8
Jose Soto <jsoto@megatech.la>
2021-10-11 12:32:47 -0300

    asd

---
+++ tpintegrador/assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+function calcular(){
+    let cantidad = parseFloat(document.getElementById('txtcant').value );
+    let categoria = parseFloat(document.getElementById('selcategoria').value );
+    let p = document.getElementById('total');
+    let total = cantidad * (200 - (200 * categoria))
+    p.innerHTML = total;
+
+}
