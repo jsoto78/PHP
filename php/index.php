583cce91eddbb15bca6b75ca9c50bb01eb16c125
Jose Soto <jsoto@megatech.la>
2021-11-02 08:12:34 -0300

    asd

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+//nombre, apellido, edad, hobbie, editor de código preferido, sistema operativo que utiliza
+$inicio = "<p>";
+$fin = "</p>";
+echo($inicio.'Nombre: Jose'.$fin);
+echo($inicio.'Apellido: Soto'.$fin) ;
+echo($inicio.'Hobbie: Futbol'.$fin) ;
+echo($inicio.'Editor de codigo: Visual Studio'.$fin) ;
+echo($inicio.'Sistema operativo: macOs Monterrey'.$fin); 
+
+// php -S localhost:9000
+?>
